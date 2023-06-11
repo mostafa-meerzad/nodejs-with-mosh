90fada4078af7c4a986db4019a82bf5318130dee
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-06-11 11:19:13 +0430

    Getting started with node

---
+++ first-step/sayHello.js
@@ -0,0 +1,9 @@
+function sayHello (name){
+    console.log("Hello " + name)
+}
+
+sayHello("Mostafa")
+
+// if you run the line below it throws an exception, because window object is only available in a browser.
+
+// console.log(window)
